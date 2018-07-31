51352c98597fdf2a973cbc6ee18889ca3abb7936
gazdagergo <info@gazdagergo.com>
2018-07-31 11:29:20 +0200

    clone works

--- src/index.js
+++ src/index.js
@@ -2,17 +2,33 @@ const myObj = {
   name: "Paddy",
   address: {
     town: "Lerum",
-    country: "Sweden"
-  }
+    country: "Sweden",
+    buildings: [34, { floor: 5 }]
+  },
+  badges: ["foo", { b1: "bar" }]
 };
 
-function deepClone(obj) {
-  Object.keys(obj).map(k => {
-    if (typeof obj[k] === "object") {
-      deepClone(obj[k]);
-    }
-    console.log(k);
-  });
+function isArray(obj) {
+  return Object.prototype.toString.call(obj) === "[object Array]";
 }
 
-deepClone(myObj);
+function deepClone(obj, isArr = false) {
+  var newObject = {};
+  if (isArr) newObject = [];
+  Object.keys(obj).map(k => {
+    if (typeof obj[k] === "object") {
+      if (isArray(obj[k])) {
+        Object.assign(newObject, { [k]: deepClone(obj[k], true) });
+      } else {
+        Object.assign(newObject, { [k]: deepClone(obj[k]) });
+      }
+    } else {
+      Object.assign(newObject, { [k]: obj[k] });
+    }
+  });
+  return newObject;
+}
+
+var n = deepClone(myObj);
+console.log(myObj);
+console.log(n);
