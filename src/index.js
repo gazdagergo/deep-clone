563598aef80540f65800fd9968d9fce06270a6de
gazdagergo <info@gazdagergo.com>
2018-07-31 14:29:14 +0200

    handle arrays

--- src/index.js
+++ src/index.js
@@ -8,27 +8,40 @@ const myObj = {
   badges: ["foo", { b1: "bar" }]
 };
 
+const myArr = ["foo", { bar: { foo: "bar" } }];
+
 function isArray(obj) {
   return Object.prototype.toString.call(obj) === "[object Array]";
 }
 
-function deepClone(obj, isArr = false) {
-  var newObject = {};
-  if (isArr) newObject = [];
-  Object.keys(obj).map(k => {
-    if (typeof obj[k] === "object") {
-      if (isArray(obj[k])) {
+function deepClone(obj) {
+  if (isArray(obj)) {
+    var newArray = [];
+    obj.map(item => {
+      if (typeof item === "object") {
+        newArray.push(deepClone(item));
+      } else {
+        newArray.push(item);
+      }
+    });
+    return newArray;
+  } else {
+    var newObject = {};
+    Object.keys(obj).forEach(k => {
+      if (typeof obj[k] === "object") {
         Object.assign(newObject, { [k]: deepClone(obj[k], true) });
       } else {
-        Object.assign(newObject, { [k]: deepClone(obj[k]) });
+        Object.assign(newObject, { [k]: obj[k] });
       }
-    } else {
-      Object.assign(newObject, { [k]: obj[k] });
-    }
-  });
-  return newObject;
+    });
+    return newObject;
+  }
 }
 
 var n = deepClone(myObj);
 console.log(myObj);
 console.log(n);
+
+var a = deepClone(myArr);
+console.log(myArr);
+console.log(a);
